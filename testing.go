5040e2ff0824c6d1f83ce2e7a45241a58ad43e06
cgliu-create <chrisliu2438@gmail.com>
2020-08-07 14:45:18 -0400

    duplicate objs

--- testing.go
+++ testing.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 
 	"github.com/cgliu-create/TortoiseHare/algo"
+	"github.com/cgliu-create/TortoiseHare/duplicateobj"
 )
+
+//Point - test struct
+type Point struct {
+	X, Y int
+}
 
 func main() {
 	fmt.Println("hello")
 	ex := []int{4, 3, 2, 7, 5, 8, 3, 6, 1}
 	fmt.Println(algo.FindDuplicate(ex))
+	var ex2 = []interface{}{Point{1, 2}, Point{2, 5}, Point{3, 3}, Point{1, 2}, Point{5, 6}, Point{2, 5}}
+	fields := []string{"X", "Y"}
+	fmt.Println(duplicateobj.FindDuplicateObj(fields, ex2))
 }
